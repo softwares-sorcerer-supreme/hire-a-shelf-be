d8a56a50b986822bc0278f65222192018e426d1c
Nguyễn Minh Thái <tv.fepfer01@gmail.com>
2023-02-19 21:43:11 +0700

    Test

--- src/main/java/com/example/shelve/services/impl/StoreServiceImpl.java
+++ src/main/java/com/example/shelve/services/impl/StoreServiceImpl.java
@@ -3,7 +3,9 @@ package com.example.shelve.services.impl;
 import com.example.shelve.repository.StoreRepository;
 import com.example.shelve.services.StoreService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class StoreServiceImpl implements StoreService {
 
     @Autowired
