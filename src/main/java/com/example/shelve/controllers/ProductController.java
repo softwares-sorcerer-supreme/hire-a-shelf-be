a738e3d72edfa723a00284877115b0d2978f5f52
Nguyễn Minh Thái <tv.fepfer01@gmail.com>
2023-03-09 17:06:38 +0700

    Create get products by filter API

--- src/main/java/com/example/shelve/controllers/ProductController.java
+++ src/main/java/com/example/shelve/controllers/ProductController.java
@@ -1,6 +1,8 @@
 package com.example.shelve.controllers;
 
 import com.example.shelve.dto.request.ProductRequest;
+import com.example.shelve.dto.response.APIResponse;
+import com.example.shelve.dto.response.CampaignResponse;
 import com.example.shelve.dto.response.ProductResponse;
 import com.example.shelve.entities.Product;
 import com.example.shelve.services.ProductService;
@@ -22,6 +24,14 @@ public class ProductController {
     @GetMapping
     public ResponseEntity<List<ProductResponse>> getAllProduct() {
         return new ResponseEntity<>(productService.getAllProduct(), HttpStatus.OK);
+    }
+
+    @GetMapping("/brand")
+    public APIResponse<List<ProductResponse>> getCampaignByBrand(@RequestParam(required = false, defaultValue = "") String keyword,
+                                                                 @RequestParam(required = false, defaultValue = "0") long brandId,
+                                                                 @RequestParam(value = "page", required = false, defaultValue = "0") int page,
+                                                                 @RequestParam(required = false) List<Long> categoriesId) {
+        return productService.getBrandProducts(brandId, keyword, page, categoriesId);
     }
 
     @PostMapping
--- src/main/java/com/example/shelve/repository/ProductRepository.java
+++ src/main/java/com/example/shelve/repository/ProductRepository.java
@@ -1,7 +1,33 @@
 package com.example.shelve.repository;
 
+import com.example.shelve.entities.Category;
 import com.example.shelve.entities.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    @Query(value = "SELECT p FROM Product p join p.category" +
+            " WHERE p.category IN :categories" +
+            " AND (LOWER(p.name) LIKE %:keyword%)" +
+            " AND p.brand.id = :brandId")
+    Page<Product> findByKeywordWithFilter(
+            @Param("categories") List<Category> categories,
+            @Param("keyword") String keyword,
+            @Param("brandId") long brandId,
+            Pageable pageable
+    );
+
+    @Query(value = "SELECT p FROM Product p join p.category" +
+            " WHERE p.category IN :categories" +
+            " AND (LOWER(p.name) LIKE %:keyword%)")
+    Page<Product> findByKeywordWithFilterAdmin(
+            @Param("categories") List<Category> categories,
+            @Param("keyword") String keyword,
+            Pageable pageable
+    );
 }
