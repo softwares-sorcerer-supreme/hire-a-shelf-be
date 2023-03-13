3b7207e28774319f9d24d022dd8d5a842fdeba3a
Tuan Be <tuanblep0298@gmail.com>
2023-03-13 17:00:03 +0700

    add push notification to multiple devices

--- src/main/java/com/example/shelve/services/FirebaseMessagingService.java
+++ src/main/java/com/example/shelve/services/FirebaseMessagingService.java
@@ -2,11 +2,16 @@ package com.example.shelve.services;
 
 import com.example.shelve.dto.request.PushNotificationRequest;
 import com.example.shelve.dto.response.NotificationResponse;
+import com.google.firebase.messaging.FirebaseMessagingException;
+
+import java.util.List;
 
 public interface FirebaseMessagingService {
 
     void sendNotificationToToken(PushNotificationRequest request);
 
     NotificationResponse getNotificationByAccountId(Long id);
+
+    void sendNotification(String title, String body, List<String> tokens) throws FirebaseMessagingException;
 
 }
