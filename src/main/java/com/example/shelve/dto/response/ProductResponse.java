6315e96005e9955624a58097b558ccea0b23fad1
Nguyễn Minh Thái <tv.fepfer01@gmail.com>
2023-03-17 02:39:17 +0700

    Return applier for campaign and product

--- src/main/java/com/example/shelve/dto/response/ProductResponse.java
+++ src/main/java/com/example/shelve/dto/response/ProductResponse.java
@@ -1,16 +1,12 @@
 package com.example.shelve.dto.response;
 
-import com.example.shelve.entities.*;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
 import java.io.Serializable;
 import java.math.BigDecimal;
-import java.util.Set;
 
 @Data
 @AllArgsConstructor
@@ -26,6 +22,6 @@ public class ProductResponse implements Serializable {
     private int quantity;
     private BigDecimal price;
     private String imgURL;
-//    private CategoryResponse category;
+    private CategoryResponse categoryResponse;
 
 }
