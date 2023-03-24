8f45cbfb6a16e881f65460909b41ded4cc06b543
Nguyễn Minh Thái <tv.fepfer01@gmail.com>
2023-03-24 16:31:49 +0700

    Fix bugs, authorizaiton

--- src/main/java/com/example/shelve/mapper/StoreMapper.java
+++ src/main/java/com/example/shelve/mapper/StoreMapper.java
@@ -1,16 +1,23 @@
 package com.example.shelve.mapper;
 
+import com.example.shelve.dto.response.ShelvesResponse;
 import com.example.shelve.dto.response.StoreResponse;
 import com.example.shelve.entities.Store;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Component
 public class StoreMapper {
 
     @Autowired
     private LocationMapper locationMapper;
-
+    @Autowired
+    private ShelvesMapper shelvesMapper;
     public StoreResponse toStoreResponse(Store store) {
         if(store == null)
             return null;
@@ -24,6 +31,9 @@ public class StoreMapper {
                 .participateDate(store.getParticipateDate())
                 .status(store.isStatus())
                 .location(locationMapper.toLocationResponse(store.getLocation()))
+                .shelves(store.getShelves().stream()
+                        .map(shelvesMapper::toShelveResponse)
+                        .collect(Collectors.toSet()))
                 .build();
     }
 
